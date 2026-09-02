2828189a7d4d40e6edeb56e50aee8e897f61af7f
BAGGS271 <124683933+BAGGS271@users.noreply.github.com.>
2026-09-02 12:14:23 +0100

    Fix post user association and migration version

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -8,7 +8,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.repository.UserRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.List;
 
@@ -17,6 +20,9 @@ public class PostsController {
 
     @Autowired
     PostRepository repository;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/posts")
     public String index(Model model) {
@@ -27,8 +33,21 @@ public class PostsController {
     }
 
     @PostMapping("/posts")
-    public RedirectView create(@ModelAttribute Post post) {
+    public RedirectView create(
+            @ModelAttribute Post post,
+            Authentication authentication
+    ) {
+        OAuth2User oauthUser = (OAuth2User) authentication.getPrincipal();
+
+        String username = oauthUser.getAttribute("email");
+
+        User user = userRepository.findUserByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        post.setUser(user);
+
         repository.save(post);
+
         return new RedirectView("/posts");
     }
     //Edit and Delete Post functionality
