5c5cfc57b1b6322aab8c74d6108e57cb3120b1a7
Megan <269440206+megansangster2001@users.noreply.github.com>
2026-09-02 12:33:30 +0100

    routed mapping to home instead of posts

--- src/main/java/com/makersacademy/acebook/controller/HomeController.java
+++ src/main/java/com/makersacademy/acebook/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.makersacademy.acebook.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = "/")
-	public RedirectView index() {
-		return new RedirectView("/posts");
+	@GetMapping("/")
+public String index() {
+		return "home/index";
 	}
 }
