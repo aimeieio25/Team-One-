6211f31e9b1c4343e050235b2a77efdad9805b29
aimeieio25 <meighanaimee04@gmail.com>
2026-09-03 12:22:46 +0100

    Add post date and time and V6 migration for this

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -2,6 +2,7 @@ package com.makersacademy.acebook.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import java.time.LocalDateTime;
 
 @Data
 @Entity
@@ -13,6 +14,9 @@ public class Post {
     private Long id;
 
     private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 
     @ManyToOne
     @JoinColumn(name = "user_id")
@@ -27,5 +31,9 @@ public class Post {
     public Post(String content, User user) {
         this.content = content;
         this.user = user;
+    }
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
     }
 }
