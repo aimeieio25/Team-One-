141dd9031c0f7af1fa0e09f0e4afeebd681810e7
KPeden-dev <KPeden-dev@users.noreply.github.com>
2026-09-03 19:20:58 +0100

    Clean up on migration

--- src/main/resources/db/migration/V7__add_handle_to_users.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE users
-    ADD COLUMN handle VARCHAR(50);
-
-UPDATE users
-SET handle = SPLIT_PART(username, '@', 1);
-
-ALTER TABLE users
-    ADD CONSTRAINT users_handle_unique UNIQUE (handle);
